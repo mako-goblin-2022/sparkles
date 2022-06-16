82322ce453594d86e3ece56cbce855c7f5e2df31
kate-stretton <81283807+kate-stretton@users.noreply.github.com>
2022-06-16 20:38:34 +1200

    Add css for canvas

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,7 +3,7 @@ import Pixel from './Pixel'
 
 const App = () => {
   const pixels = Array.from({length: 1200}, () => [<Pixel /> ])
-  return <div>
+  return <div id='canvas'>
     {pixels}
   </div>
 }
