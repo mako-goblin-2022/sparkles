7290d5386f9d6d946d94a9ced044a62ea5e368ce
Daniela von Arb <103089806+danielavonarb@users.noreply.github.com>
2022-06-16 20:01:25 +1200

    Create Pixel style

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -1,9 +1,10 @@
 import React, { useState} from 'react'
 
 const Pixel = () => {
+  const [style, setStyle] = useState({backgroundColor: '#800080', height: '20px', width: '20px'})
   
   return (
-  <div></div>
+  <div style={style}></div>
 
 )}
 
