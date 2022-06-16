898bb099bcf2bfb4281cd7fa0ce2230b6e75242d
Kate Stretton <katestretton@Kates-MacBook-Air.local>
2022-06-17 09:45:09 +1200

    Add header and footer components

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import Pixel from './Pixel'
+import Header from './Header'
+import Footer from './Footer'
 
 const App = () => {
   const pixels = Array.from({length: 1200}, () => [<Pixel /> ])
-  return <div id='canvas'>
+  return( 
+  <>
+ <Header/>
+  <div id='canvas'>
     {pixels}
   </div>
-}
+  <Footer/>
+  </>
+  )}
 
 export default App
---
+++ client/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+      <p>✨Sparkle Canvas✨</p>
+      </header>
+  )
+}
+
+export default Header
