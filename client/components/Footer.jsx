485ad822d586fcd0d4271fb74c3295558d145bc2
mere-mansell <102892063+mere-mansell@users.noreply.github.com>
2022-06-17 11:30:55 +1200

    add reset function to button

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
 const Footer = () => {
+
+  const refreshPage = () => {
+    window.location.reload()
+  }
+
   return (
     <footer>
-      <button>reset</button>
+      <button onClick={refreshPage}>Reset</button>
     </footer>
   )
 }
