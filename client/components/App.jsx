6166c91e06a0bef64f8e96d48b8703a38c0c9605
mangobluebird <103153488+nicole-ferma@users.noreply.github.com>
2022-06-16 20:15:14 +1200

    multiply pixel component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 import Pixel from './Pixel'
 
 const App = () => {
+  const pixels = Array.from({length: 1200}, () => [<Pixel /> ])
   return <div>
-    <Pixel /> 
+    {pixels}
   </div>
 }
 
